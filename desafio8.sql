1749ecf4d37fb2090d2e99c213cb41a4e7a13f67
William Igor <williamigorsilva@gmail.com>
2020-07-16 17:13:08 -0300

    second part

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,7 @@
 ALTER TABLE northwind.products
-ADD A varchar(255),
-ADD Trybe varchar(255),
-ADD eh varchar(255);
+ADD A INT,
+ADD Trybe varchar(50),
+ADD eh INT;
 
 INSERT INTO northwind.products (A, Trybe, eh) VALUES (5 + 6, 'de', 2 + 8);
+SELECT A, TRYBE, eh FROM northwind.products;
