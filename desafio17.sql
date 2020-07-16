3ba7ef21cc56c547ddbfde75d9b2db7d09f4ddd1
Rhian Castro <rhi.castro@gmail.com>
2020-07-16 17:16:51 -0300

    17

--- desafio17.sql
+++ desafio17.sql
@@ -1,6 +1,2 @@
-SELECT 
-    supplier_id
-FROM
-    northwind.purchase_orders
-WHERE
-    supplier_id IN (1 , 6, 5, 7);
+SELECT id, supplier_id FROM northwind.purchase_orders
+WHERE supplier_id IN (1, 3, 5, 7);
