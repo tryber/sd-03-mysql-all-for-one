065ff28cf2efa597686f0fcb1ec361b4405c1727
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-16 16:39:43 -0300

    some of second exercise

---
+++ desafio10.sql
@@ -0,0 +1,2 @@
+SELECT created_by FROM northwind.purchase_orders
+	WHERE created_by >= 3;
--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,1 @@
-SELECT 5 + 6 AS A, 'de' AS TRYBE, 2 + 8 'eh';
+SELECT 5 + 6 AS A, 'de' AS 'Trybe', 2 + 8 'eh';
