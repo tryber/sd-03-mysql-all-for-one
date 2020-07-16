93cfc19e53181018210b5f76fc660bd8514d97f5
fabemiliano <fab.emiliano@gmail.com>
2020-07-16 16:33:36 -0300

    fixed some errors

--- desafio14.sql
+++ desafio14.sql
@@ -1,2 +1,2 @@
-select hour(submitted_date) as submitted_hour from purchase_orders
+select supplier_id from purchase_orders
 where supplier_id between 1 and 3;
--- desafio21.sql
+++ desafio21.sql
@@ -1,3 +1,2 @@
-select * from order_details;
 insert into order_details(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 values(69, 80, 15.0000, 15.0000, 0, 2, null, null, 129), (69, 80, 15.0000, 15.0000, 0, 2, null, null, 129);
