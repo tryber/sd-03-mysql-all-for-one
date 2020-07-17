9079999f6401f0abd922999b356bb7694ec4c14d
potew <potew0@gmail.com>
2020-07-17 12:43:22 -0300

    Update 5 & 10

--- desafio5.sql
+++ desafio5.sql
@@ -1,3 +1,4 @@
--- 5. Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro. Obs.: não use where ou order by.
-SELECT id FROM northwind.products
-LIMIT 9 OFFSET 3;
+-- 5. Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro, 
+-- incluindo tanto um quanto o outro. Obs.: não use where ou order by.
+SELECT * FROM northwind.products
+LIMIT 10 OFFSET 3;
