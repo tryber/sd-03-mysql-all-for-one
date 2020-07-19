da103143c00bc86a981e27b05bc587a2cd890f59
Gabryel <gabryel.ryba@gmail.com>
2020-07-19 14:43:12 -0300

    codeclimate issues fix

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,3 @@
- SELECT COUNT(*) AS orders_count FROM northwind.orders
-WHERE employee_id in (5, 6) AND shipper_id = 2;
+SELECT COUNT(*) AS orders_count FROM northwind.orders
+WHERE employee_id in (5, 6)
+AND shipper_id = 2;
